5ba05145838766501d2182f7ba5a88634ef4ef54
ZhasulanKosbayev <zhasulan.kosbayev@gmail.com>
2026-09-04 18:28:19 +0500

    Create MainActivity with basic structure
    Initial implementation of MainActivity with onCreate method.

---
+++ app/src/main/java/com/engineering/uutcalc/ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.engineering.uutcalc.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Здесь подключается пользовательский интерфейс приложения
+    }
+}
